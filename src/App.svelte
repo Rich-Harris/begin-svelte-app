80e894a627fb92bb34670e2e4eb57d64e5adac2c
Rich Harris <richard.a.harris@gmail.com>
2020-02-17 16:30:54 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -8,11 +8,29 @@
   })
 </script>
 
+<main>
+  <h1>{message}</h1>
+  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
 <style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
   h1 {
-    color: purple;
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+	    max-width: none;
+    }
   }
 </style>
-
-<h1>{message}</h1>
-<h2>Change me!</h2>
